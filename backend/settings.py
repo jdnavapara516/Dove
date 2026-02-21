83520c0ef61f663ee7bafc2f18b011824ddebc97
navap <navaparajenil349@gmail.com>
2026-02-21 11:27:26 +0530

    downgraded django to 5.2 for pythonanywhere

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vpk8jan@4c%v&n*ppg=&16xb!d%pqj-e65**0nps2%x&2foirl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['jenilnavapara.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
